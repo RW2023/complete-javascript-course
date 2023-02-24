5670b7fd5ea6f13c600f3809f130d5293c28597a
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-02-24 00:56:46 -0500

    create js test function

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+function jsTest(){
+ console.log("Yow Yow. JavaScript De Yah!");   
+ console.log("weh you a seh Ryan? Yeh man everything connect up right an you a gwaan good man! how you mean!?")
+}
+
